ec7f21b2425d716703c5928f518436d6217bacbc
Luiz Spies <luiz.spies@gmail.com>
2026-06-14 14:24:38 +0200

    chore: fix unused import in bench

--- benches/cpu_gemv.rs
+++ benches/cpu_gemv.rs
@@ -1,5 +1,5 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use rocmforge::cpu::ops::{gemv_q4_0, gemv_q4_0_q8_0};
+use rocmforge::cpu::ops::gemv_q4_0;
 
 /// Benchmark Q4_0 × Q8_0 GEMV.
 fn bench_q4_0_q8_0(c: &mut Criterion) {
